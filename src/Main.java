dced0020dbf4af56d195d13b4f8678d155395f8c
Ruslan Popov <ruslan_popov_2014@inbox.ru>
2023-02-20 20:15:12 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+            String[] arr = new String[]{"abc", "abd", "def", "acf", "abc", "afx"};
+
+             ISum iSum = (v) -> System.out.println(v);
+             Reality reality = new Reality(iSum);
+             reality.execute();
+
+    }
+
+@FunctionalInterface
+     interface ISum <T> {
+     int sum  (T abc, T arr);
     }
 }
+

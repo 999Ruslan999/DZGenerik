5c44172ee4477286931e1532f44f8aac1342ece1
Ruslan Popov <ruslan_popov_2014@inbox.ru>
2023-02-23 18:51:56 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,39 @@
 public class Main {
     public static void main(String[] args) {
 
+        String[] array = {"abc", "abd", "def", "acf", "afx", "abc"};
+        Integer[] arrayInt = {34, 56, 78, 34, 3, 34, 34};
 
 
-            String[] arr = new String[]{"abc", "abd", "def", "acf", "abc", "afx"};
+        ISum search = ((arr, str) -> {
+            int count = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == str) {
+                    count++;
+                }
+            }
+            return count;
+        });
+        int count = search.sum(array, "abc");
+        System.out.println(count);
 
-             ISum iSum = (v) -> System.out.println(v);
-             Reality reality = new Reality(iSum);
-             reality.execute();
+        ISum number = ((arr, str) -> {
+            int count1 = 0;
+            for (int i = 0; i < arrayInt.length; i++) {
+                if (arrayInt[i] == str) {
+                    count1++;
+                }
+            }
+            return count1;
+        });
 
+        int count1 = number.sum(arrayInt, 34);
+        System.out.println(count1);
     }
 
-@FunctionalInterface
-     interface ISum <T> {
-     int sum  (T abc, T arr);
+    @FunctionalInterface
+    interface ISum<T> {
+        int sum(T abc, T array);
     }
 }
 
